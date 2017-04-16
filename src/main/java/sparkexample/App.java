321f952358c75806937dd676be27787e7fbae368
Stephan Rayner <stephan@echosec.net>
2017-04-15 20:25:46 -0700

    RD-30: Initial Commit

---
+++ src/main/java/sparkexample/App.java
@@ -0,0 +1,30 @@
+package sparkexample;
+
+import sparkexample.RNNSentiment;
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+import java.sql.SQLOutput;
+
+// import java.util.HashMap;
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonObject;
+
+public class App {
+    static RNNSentiment sentiment = new RNNSentiment();
+    public static void main(String[] args) {
+
+        get("/", (request, response) -> {
+            return "I love cheese!";
+        });
+
+        post("/api/v0/sentiment", (request, response) -> {
+            String body = request.body();
+            JsonObject jsonBody = new JsonParser().parse(body).getAsJsonObject();
+            return sentiment.compute(jsonBody.get("text").toString());
+        });
+
+    }
+
+}
